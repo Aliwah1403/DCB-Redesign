56da333472733bf89aacb20472f7cfdfbccdc72d
Aliwah <aliwahcurtis5@gmail.com>
2024-12-14 23:52:17 +0400

    refactor(blogsPage): Added a link to the blog post image and made changes to the padding and margin to the top and bottom of each post

--- src/pages/Blogs page/Blogs.jsx
+++ src/pages/Blogs page/Blogs.jsx
@@ -166,20 +166,22 @@ const Blogs = () => {
         </section>
 
         {/* dummy content */}
-        <div className="bg-white py-12 sm:py-24">
+        <div className="bg-white py-12 sm:py-12">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16  pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16  pt-10 sm:mt-12 sm:pt-12 lg:mx-0 lg:max-w-none lg:grid-cols-3">
               {posts.map((post) => (
                 <article
                   key={post.id}
                   className="flex max-w-xl flex-col items-start justify-between"
                 >
-                  <div className="w-full aspect-[16/9] mb-4 overflow-hidden rounded-lg">
-                    <img
-                      src={post.imageUrl}
-                      alt={post.title}
-                      className="w-full h-full object-cover"
-                    />
+                  <div className="w-full aspect-[16/9] mb-4 overflow-hidden rounded-lg ">
+                    <a href={post.href}>
+                      <img
+                        src={post.imageUrl}
+                        alt={post.title}
+                        className="w-full h-full object-cover"
+                      />
+                    </a>
                   </div>
                   <div className="flex items-center gap-x-4 text-xs">
                     <time dateTime={post.datetime} className="text-gray-500">
